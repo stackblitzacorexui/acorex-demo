cc5c9014308b46b29db4876174923db80fc3a049
mehrabix <mehrabi@post.com>
2021-12-27 09:22:52 +0330

    button initial

---
+++ src/app/demos/button/button-disabled/button-disabled.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-decoration',
+  templateUrl: './button-disabled.page.html',
+})
+export class ButtonDisabledPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/demos/button/button-sizes/button-sizes.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-sizes',
+  templateUrl: './button-sizes.page.html',
+})
+export class ButtonSizesPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
