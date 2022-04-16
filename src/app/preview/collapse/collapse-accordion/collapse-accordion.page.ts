55991dcd20cf067e479c8882eadffd627319bba4
m.saberi <m.saberi@demisco.com>
2022-04-16 08:55:05 +0430

    collapse

---
+++ src/app/preview/collapse/collapse-accordion/collapse-accordion.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collapse-accordion.',
+  templateUrl: './collapse-accordion.page.html',
+})
+export class NumberBoxDisabledPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
